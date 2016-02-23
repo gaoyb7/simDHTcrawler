83184e5f286a139eaf6ed0ccbf67b82b540233ad
gaoyb7 <gaoyb7@gmail.com>
2016-02-23 21:43:04 +0800

    add fetchMetadata.py

---
+++ fetchMetadata.py
@@ -0,0 +1,13 @@
+import socket
+
+from btdht import gen_node_id
+from struct import pack, unpack
+
+PROTOCOL_ID = b"BitTorrent protocol"
+
+def send_handshake(s, infohash):
+    nid = gen_node_id()
+    header = chr(len(PROTOCOL_ID)).encode() + PROTOCOL_ID
+    reserved = b"\x00\x00\x00\x00\x00\x10\x00\x00"
+    msg = header + reserved + infohash + nid
+    s.send(msg)
--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import bencodepy
 import codecs
+import socket
 
 from btdht import btdht
+from fetchMetadata import send_handshake
 
 class Master(object):
     def log(self, infohash, name, address):
         print("%s %s" % (codecs.encode(infohash, "hex_codec").decode(), name.decode()))
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(5)
+        s.connect(address)
+
+        send_handshake(s, infohash)
+        packet = s.recv(4096)
+        print(packet)
 
 
 if __name__ == "__main__":
