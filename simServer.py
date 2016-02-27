4bac6d8c7561841a1078fc9f5749e6735ff3ccda
gaoyb7 <gaoyb7@gmail.com>
2016-02-27 17:40:06 +0800

    update simServer.py

--- simServer.py
+++ simServer.py
@@ -1,25 +1,37 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request
 from sqlalchemy import create_engine, text
 
 app = Flask(__name__)
 
 engine = create_engine("postgresql://gaoyb7@localhost/dht")
 conn = engine.connect()
+search_form = \
+    """
+    <form method="GET" action="/search">
+        <input id="kw" name="kw" />
+        <button type="submit">search</button>
+    </form>
+    """
 
 @app.route("/")
-def hello_world():
-    return "Hello World!"
+def main_page():
+    return "Total torrent:  " + str(torrent_count()) + "</br>" + search_form
 
 
 @app.route("/about")
 def about():
     return "A simple Torrent Search Engine based on DHT protocol"
 
-@app.route("/search/<name>")
-def search(name):
+
+@app.route("/search")
+def search():
+    msg = "Total torrent:  " + str(torrent_count()) + "</br>" + search_form
+    msg += "</br>"
+    msg += "Result: </br>"
+
+    name = request.args.get('kw')
     cmd = "select * from hash_tab where upper(name) like " + "'%" + name.upper() + "%'"
     r = conn.execute(text(cmd))
-    msg = ""
     for item in r:
         msg += "Name: " + item[1] + "</br>"
         msg += "Magnet: " + to_magnet(item[0], item[1]) + "</br>"
@@ -31,6 +43,15 @@ def to_magnet(infohash, name):
     magnet = "magnet:?xt=urn:btih:" + infohash
     magnet += "&dn=" + name
     return magnet
+
+
+def torrent_count():
+    cmd = "select count(*) from hash_tab"
+    r = conn.execute(cmd)
+    for x in r:
+        return x[0]
+
+
 
 if __name__ == "__main__":
     app.debug = True
